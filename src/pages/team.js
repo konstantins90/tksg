a3b640bc3c0d32a40ee5ac6c8ee7ae218be73aab
Konstantin Smetana <konstantinsmetana@gmail.com>
2024-11-12 08:52:40 +0100

    angeliki raus

--- src/pages/team.js
+++ src/pages/team.js
@@ -194,28 +194,6 @@ export default function Projektentwicklung({ data }) {
                         </div>
                     </div>
                 </div>
-                <div className="person">
-                    <div className="image overflow-hidden rounded-full max-w-[250px] max-h-[250px]">
-                        <StaticImage
-                            src="../images/team/Turnkey-Solar-Angeliki-Foukas.jpg"
-                            alt="Angeliki Foukas von Turn Key Solar GmbH"
-                            placeholder="blurred"
-                            layout="constrained"
-                            width={250}
-                            quality={90}
-                            imgStyle={{ 'object-position': 'right center' }}
-                            objectFit="cover"
-                            class="w-full posistion"
-                        />
-                    </div>
-                    <div className="name text-center py-5">
-                        <div className="font-bold text-lg">Angeliki Foukas</div>
-                        <div className="italic text-gray-400">Duale Stundentin</div>
-                        <div className="mt-2">
-                            <a href="tel:+4952549478564">T: +49 5254 94785 64</a>
-                        </div>
-                    </div>
-                </div>
             </div>
             <div className="flex flex-wrap gap-20 justify-center">
                 <div className="person">
